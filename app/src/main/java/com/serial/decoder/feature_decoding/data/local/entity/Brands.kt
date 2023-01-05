28b5493f27c3e77691d3f980f046c26db4512241
devMohaned <businessmohaned@gmail.com>
2023-01-05 13:08:47 +0200

    Added @Keep in Brands.kt

--- app/src/main/java/com/serial/decoder/feature_decoding/data/local/entity/Brands.kt
+++ app/src/main/java/com/serial/decoder/feature_decoding/data/local/entity/Brands.kt
@@ -1,5 +1,15 @@
 package com.serial.decoder.feature_decoding.data.local.entity
 
+import androidx.annotation.Keep
+
+/*
+When we create an enum class, the compiler will create instances (objects) of each enum constants.
+Also, all enum constant is always public static final by default. */
+
+/* @Keep is used to keep this class from being renamed,thus preventing an error
+ in production when accessing @Brands properties, this is made due to
+ minifyEnabled true in the build.gradle app module */
+@Keep
 enum class Brands {
     SAMSUNG,
     LG,
